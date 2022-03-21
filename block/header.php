66267f38d89d77ebeb2b254e09ae341354ce6604
memoryspeak <iliya.abdrakhimov@gmail.com>
2022-03-21 10:52:37 +0300

    new strucure

---
+++ block/header.php
@@ -0,0 +1,12 @@
+<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/">Mehen&#128013;<?php echo $mehenname; ?></a>
+
+  <div class="navbar-nav">
+    <div class="nav-item text-nowrap">
+      <a class="nav-link me-0 px-3" href="#">&#127922; <span style="color: #6B8E23"><b>123</b></span></a>
+    </div>
+  </div>
+  <button class="navbar-toggler d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+</header>
---
+++ block/menu.php
@@ -0,0 +1,85 @@
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+      <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/">
+              <span>&#127968;</span>
+              Mehenboard
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/rules/rules.php">
+              Game Rules
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/mehenProject/mehenProject.php">
+              Settings
+            </a>
+          </li>
+          <!--<li class="nav-item">
+            <a class="nav-link" href="/mehenProject/mehenProject.php">
+              <span>&#128101;</span>456
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/mehenProject/mehenProject.php">
+              <span>&#127922;</span>123
+            </a>
+          </li>-->
+        </ul>
+
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+          <span>Game</span>
+        </h6>
+        <ul class="nav flex-column mb-2">
+          <li class="nav-item">
+            <a class="nav-link" href="#" onclick="startGame('oppositeEachOver')">
+              <span>&#128490;</span>
+              Opposite Each Over
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span>&#128187;</span>
+              With Robot
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span>&#128423;</span>
+              Online Game
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span>&#128250;</span>
+              Mehen TV
+            </a>
+          </li>
+        </ul>
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+          <span>Mehen Project</span>
+        </h6>
+        <ul class="nav flex-column mb-2">
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Support
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Mehen Shop
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              About
+            </a>
+          </li>
+        </ul>
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+          <span>@2021 - <?php echo date('Y'); ?></span>
+        </h6>
+      </div>
+    </nav>
