395da95bbf38525412b503eb59921d0515f02ed3
memoryspeak <iliya.abdrakhimov@gmail.com>
2022-03-22 22:56:34 +0300

    validation

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,14 +1,17 @@
 <?php
-$db_host = "127.0.0.1";
-$db_user = "nuxa";
-$db_pass = "memoryspeak";
-$db_name = "mehendatabase";
+$db_ini_array = parse_ini_file('../db.ini');
+
+$db_host = $db_ini_array["db_host"];
+$db_user = $db_ini_array["db_user"];
+$db_pass = $db_ini_array["db_pass"];
+$db_name = $db_ini_array["db_name"];
 
 try {
     $db_connect = new PDO("mysql:host={$db_host};dbname={$db_name}",$db_user,$db_pass);
     $db_connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    /*$connect->exec('SET NAMES "utf8"');*/
+    //$connect->exec('SET NAMES "utf8"');
 } catch(PDOException $exception) {
     echo $exception->getMessage();
 };
+
 ?>
--- sign/registration.php
+++ sign/registration.php
@@ -1,6 +1,16 @@
 <?php
 session_start();
 
-include_once "registration.html";
+require_once "../php/db_connect.php";
+//require_once "../php/get_users.php";
 
+try {
+    $all_users = $db_connect->prepare("SELECT `username` FROM `users`");
+    $all_users->execute();
+    include_once "registration.html";
+} catch(PDOException $exception) {
+    //$_SESSION['all_users'] = $exception;
+    //echo $exception -> getMessage();
+    include_once "/error/error.php";
+};
 ?>
